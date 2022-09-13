465fc3a93528367c01a38020832127c4ed931895
Frank Reimer <freimer@juniper.net>
2022-09-13 15:22:01 +0200

    hopefully working backend

--- tasmota/tasmota_xdrv_driver/xdrv_48_timeprop.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_48_timeprop.ino
@@ -22,6 +22,62 @@
 
 #define XDRV_48 48
 
+/*********************************************************************************************\
+ * Code to drive one or more relays in a time proportioned manner give a
+ * required power value.
+ *
+ * The following commands are accepted. <x> being the number of the timeprop
+ * to be controlled. <x> can be 1 to 4.
+ *
+ * Since Relay in Tasmota is already super flexible a direct connection has
+ * been made. Timeprop1 is controlling Relay 1 and so on.
+ *
+ * TimepropSet<x>: set the actual value from 0 to 100.
+ *
+ * TimepropTimeBase<x>: set the time base for the given timprop in minutes. Accepted
+ *                      values are between 0 and 31. A value of 0 disables the given
+ *                      timeprop.
+ *
+ * TimepropFallbackValue<x>: In case a timeprop has not received a Set command
+ *                           within a given time this value will be used.
+ *                           Set between 0 and 100. Will be rounded to one of
+ *                           8 values to keep configuration memory low.
+ *
+ * TimepropStartWithFallback: if set all timeprops will start with their fallback
+ *                            values. Global setting. Set 0 or 1.
+ *
+ * TimepropFallbackAfter: If a timeprp has not received an update within set
+ *                        time a fallback value will be set. This is setting the
+ *                        time. Set hours between 0 and 127.
+ *
+ * Remarks:
+ * The number of Timeprops is defined as MAX_TIMEPROPS. It can be increased, but
+ * each timeprop is using 8 byte of flash. before increasing make sure that
+ * free flash is available behind the timeprops. (see tasmota/include/tasmota_types.h).
+ *
+ * Setting TimepropTimeBase<x> to zero is disabling the timeprop.
+ *
+ * To keep the amount if necessary flash low timebase is limited to 31 minutes.
+ * and Fallback value is limited to 8 values between 0 and 100. (3 bit)
+ *
+ * Relay configuration is already super flexible. It has been voted against adding
+ * another layer of flexibility. The relation between timeprop and relay is
+ * fixed.
+ * Timeprop1 -> Relay1
+ * Timeprop2 -> Relay2
+ * Timeprop3 -> Relay3
+ * Timeprop4 -> Relay4
+ * ...
+ * if TimepropTimeBase<x> is set to zero no POER commands will be sent to the
+ * given relay.
+ * But make sure no Pulse is configured for one of the relays.
+ *
+ * The code has been written with floor heating in mind. normal cycle times
+ * for this use case are between 15 and 30 minutes. In case longer times are
+ * needed a global multiplier could be added but that is not implemented today.
+ *
+ **/
+
 uint32_t fallback_after_seconds = 0; // To keep settings low we set fallback time in hours. We need seconds
 bool start_with_fallback = false;    // shall we start with fallback or not
 
@@ -42,19 +98,15 @@ const char kTimepropCommands[] PROGMEM = D_PRFX_TIMEPROP "|" D_CMND_TIMEPROP_SET
 void (*const TimepropCommand[])(void) PROGMEM = {
     &CmndTimepropSet, &CmndTimepropTimeBase, &CmndTimepropFallbackvalue, &CmndTimepropStartWithFallback, &CmndTimepropFallbackAfter};
 
-// #define D_PRFX_TIMEPROP "Timeprop"
-// #define D_CMND_TIMEPROP_TIMEBASE "TimepropTimeBase1 15"
-// #define D_CMND_TIMEPROP_FALLBACKVALUE "TimepropFallbackValue1 0.4"
-// #define D_CMND_TIMEPROP_SET "TimepropSet2 0.4"
-
+/*********************************************************************************************\
+ * Init
+\*********************************************************************************************/
 void TimepropInit(void)
 {
   SyncSettings();
 
   if (start_with_fallback)
   {
-    AddLog(LOG_LEVEL_INFO, PSTR("TPR: Init timeprop_start_with_fallback is set"));
-
     for (uint32_t i = 0; i < MAX_TIMEPROPS; i++)
     {
       if (Timepropsstate[i].timebase == 0)
@@ -66,14 +118,11 @@ void TimepropInit(void)
       Timepropsstate[i].set_value = round((float)Timepropsstate[i].fallback_value * (float)100 / (float)7);
     }
   }
-  else
-  {
-    AddLog(LOG_LEVEL_INFO, PSTR("TPR: Init timeprop_start_with_fallback is not set"));
-  }
-
-  AddLog(LOG_LEVEL_INFO, PSTR("TPR: Timeprop Init"));
 }
 
+/*********************************************************************************************\
+ * Periodic
+\*********************************************************************************************/
 void TimepropEverySecond(void)
 {
 
@@ -91,20 +140,18 @@ void TimepropEverySecond(void)
     if (Timepropsstate[i].set_value == 0)
     {
       Timepropsstate[i].is_open = false;
-
-      // AddLog(LOG_LEVEL_INFO, PSTR("TPR: Timeprop TimepropEverySecond set_value is zero for Number %d"), i);
     }
     else
     {
       if (Timepropsstate[i].seconds_running == 0)
       {
         Timepropsstate[i].is_open = true;
-        AddLog(LOG_LEVEL_INFO, PSTR("TPR: Timeprop TimepropEverySecond start cycle %d with openening for %d seconds"), i, OpenSeconds(i));
+        ExecuteCommandPower(i + 1, POWER_ON, SRC_IGNORE);
       }
       if (Timepropsstate[i].is_open == true && Timepropsstate[i].seconds_running >= OpenSeconds(i))
       {
         Timepropsstate[i].is_open = false;
-        AddLog(LOG_LEVEL_INFO, PSTR("TPR: Timeprop TimepropEverySecond mid cycle %d with closing"), i);
+        ExecuteCommandPower(i + 1, POWER_OFF, SRC_IGNORE);
       }
     }
 
@@ -114,8 +161,6 @@ void TimepropEverySecond(void)
 
     if (Timepropsstate[i].last_received >= fallback_after_seconds)
     {
-      AddLog(LOG_LEVEL_INFO, PSTR("TPR: Timeprop TimepropEverySecond Fallback state %d %d %d"), i, Timepropsstate[i].last_received, fallback_after_seconds);
-
       Timepropsstate[i].set_value = Timepropsstate[i].fallback_value;
       Timepropsstate[i].last_received = 0;
     }
@@ -127,6 +172,9 @@ void TimepropEverySecond(void)
   }
 }
 
+/*********************************************************************************************\
+ * Commands
+\*********************************************************************************************/
 void CmndTimepropSet(void)
 {
 
@@ -267,7 +315,7 @@ void SyncSettings(void)
     AddLog(LOG_LEVEL_INFO, PSTR("TPR: Timeprop SyncSettings timeprop %d fallback_vlaue %d."), i, Timepropsstate[i].fallback_value);
     AddLog(LOG_LEVEL_INFO, PSTR("TPR: Timeprop SyncSettings timeprop %d timebase %d."), i, Timepropsstate[i].timebase);
   }
-  fallback_after_seconds = Settings->timeprop_cfg.fallback_time * 25; // TODO: add 60 * 60
+  fallback_after_seconds = Settings->timeprop_cfg.fallback_time * 60 * 60;
   start_with_fallback = Settings->timeprop_cfg.start_with_fallback;
 
   AddLog(LOG_LEVEL_INFO, PSTR("TPR: Timeprop SyncSettings fallback_after_seconds %d."), fallback_after_seconds);
@@ -285,7 +333,6 @@ uint8_t OpenSeconds(uint8_t index)
 /*********************************************************************************************\
  * Interface
 \*********************************************************************************************/
-
 bool Xdrv48(byte function)
 {
   bool result = false;
@@ -304,229 +351,6 @@ bool Xdrv48(byte function)
   }
   return result;
 }
-
-/*********************************************************************************************\
- * Code to drive one or more relays in a time proportioned manner give a
- * required power value.
- *
- * Given required power values in the range 0.0 to 1.0 the relays will be
- * driven on/off in such that the average power suppled will represent
- * the required power.
- * The cycle time is configurable.  If, for example, the
- * period is set to 10 minutes and the power input is 0.2 then the output will
- * be on for two minutes in every ten minutes.
- *
- * A value for actuator dead time may be provided. If you have a device that
- * takes a significant time to open/close then set this to the average of the
- * open and close times.  The algorithim will then adjust the output timing
- * accordingly to ensure that the output is not switched more rapidly than
- * the actuator can cope with.
- *
- * A facility to invert the output is provided which can be useful when used in
- * refrigeration processes and similar.
- *
- * In the case where only one relay is being driven the power value is set by
- * writing the value to the mqtt topic cmnd/timeprop_setpower_0.  If more than
- * one relay is being driven (as might be the case for a heat/cool application
- * where one relay drives the heater and the other the cooler) then the power
- * for the second relay is written to topic cmnd/timeprop_setpower_1 and so on.
- *
- * To cope with the problem of temporary wifi failure etc a
- * TIMEPROP_MAX_UPDATE_INTERVALS value is available. This can be set to the max
- * expected time between power updates and if this time is exceeded then the
- * power will fallback to a given safe value until a new value is provided. Set
- * the interval to 0 to disable this feature.
- *
- * Usage:
- * Place this file in the sonoff folder.
- * Clone the library https://github.com/colinl/process-control.git from Github
- * into a subfolder of lib.
- * In user_config.h or user_config_override.h for a single relay, include
- * code as follows:
-
- #define USE_TIMEPROP    //  include the timeprop feature (+1.2k)
-   // for single output
-   #define TIMEPROP_NUM_OUTPUTS          1       // how many outputs to control (with separate alogorithm for each)
-   #define TIMEPROP_CYCLETIMES           60      // cycle time seconds
-   #define TIMEPROP_DEADTIMES            0       // actuator action time seconds
-   #define TIMEPROP_OPINVERTS            false   // whether to invert the output
-   #define TIMEPROP_FALLBACK_POWERS      0       // falls back to this if too long betwen power updates
-   #define TIMEPROP_MAX_UPDATE_INTERVALS 120     // max no secs that are allowed between power updates (0 to disable)
-   #define TIMEPROP_RELAYS               1       // which relay to control 1:8
-
- * or for two relays:
- #define USE_TIMEPROP    //  include the timeprop feature (+1.2k)
-   // for single output
-   #define TIMEPROP_NUM_OUTPUTS          2               // how many outputs to control (with separate alogorithm for each)
-   #define TIMEPROP_CYCLETIMES           60,     10      // cycle time seconds
-   #define TIMEPROP_DEADTIMES            0,      0       // actuator action time seconds
-   #define TIMEPROP_OPINVERTS            false,  false   // whether to invert the output
-   #define TIMEPROP_FALLBACK_POWERS      0,      0       // falls back to this if too long betwen power updates
-   #define TIMEPROP_MAX_UPDATE_INTERVALS 120,    120     // max no secs that are allowed between power updates (0 to disable)
-   #define TIMEPROP_RELAYS               1,      2       // which relay to control 1:8
-
- * Publish values between 0 and 1 to the topic(s) described above
- *
-**/
-
-// #define D_CMND_TIMEPROP "timeprop_"
-// #define D_CMND_TIMEPROP_SETPOWER "setpower_"    // add index no on end (0:8) and data is power 0:1
-
-// #include "Timeprop.h"
-
-// enum TimepropCommands { CMND_TIMEPROP_SETPOWER };
-// const char kTimepropCommands[] PROGMEM = D_CMND_TIMEPROP_SETPOWER;
-
-// #ifndef TIMEPROP_NUM_OUTPUTS
-// #define TIMEPROP_NUM_OUTPUTS          1       // how many outputs to control (with separate alogorithm for each)
-// #endif
-// #ifndef TIMEPROP_CYCLETIMES
-// #define TIMEPROP_CYCLETIMES           60      // cycle time seconds
-// #endif
-// #ifndef TIMEPROP_DEADTIMES
-// #define TIMEPROP_DEADTIMES            0       // actuator action time seconds
-// #endif
-// #ifndef TIMEPROP_OPINVERTS
-// #define TIMEPROP_OPINVERTS            false   // whether to invert the output
-// #endif
-// #ifndef TIMEPROP_FALLBACK_POWERS
-// #define TIMEPROP_FALLBACK_POWERS      0       // falls back to this if too long betwen power updates
-// #endif
-// #ifndef TIMEPROP_MAX_UPDATE_INTERVALS
-// #define TIMEPROP_MAX_UPDATE_INTERVALS 120     // max no secs that are allowed between power updates (0 to disable)
-// #endif
-// #ifndef TIMEPROP_RELAYS
-// #define TIMEPROP_RELAYS               1       // which relay to control 1:8
-// #endif
-
-// static Timeprop timeprops[TIMEPROP_NUM_OUTPUTS];
-// static int relayNos[TIMEPROP_NUM_OUTPUTS] = {TIMEPROP_RELAYS};
-// static long currentRelayStates = 0;  // current actual relay states. Bit 0 first relay
-
-// struct {
-//   Timeprop timeprops[TIMEPROP_NUM_OUTPUTS];
-//   int relay_nos[TIMEPROP_NUM_OUTPUTS] = {TIMEPROP_RELAYS};
-//   long current_relay_states = 0;  // current actual relay states. Bit 0 first relay
-//   long current_time_secs = 0;  // a counter that counts seconds since initialisation
-// } Tprop;
-
-// /* call this from elsewhere if required to set the power value for one of the timeprop instances */
-// /* index specifies which one, 0 up */
-// void TimepropSetPower(int index, float power) {
-//   if (index >= 0  &&  index < TIMEPROP_NUM_OUTPUTS) {
-//     Tprop.timeprops[index].setPower( power, Tprop.current_time_secs);
-//   }
-// }
-
-// void TimepropInit(void) {
-//   // AddLog(LOG_LEVEL_INFO, PSTR("TPR: Timeprop Init"));
-//   int cycleTimes[TIMEPROP_NUM_OUTPUTS] = {TIMEPROP_CYCLETIMES};
-//   int deadTimes[TIMEPROP_NUM_OUTPUTS] = {TIMEPROP_DEADTIMES};
-//   int opInverts[TIMEPROP_NUM_OUTPUTS] = {TIMEPROP_OPINVERTS};
-//   int fallbacks[TIMEPROP_NUM_OUTPUTS] = {TIMEPROP_FALLBACK_POWERS};
-//   int maxIntervals[TIMEPROP_NUM_OUTPUTS] = {TIMEPROP_MAX_UPDATE_INTERVALS};
-
-//   for (int i = 0; i < TIMEPROP_NUM_OUTPUTS; i++) {
-//     Tprop.timeprops[i].initialise(cycleTimes[i], deadTimes[i], opInverts[i], fallbacks[i],
-//       maxIntervals[i], Tprop.current_time_secs);
-//   }
-// }
-
-// void TimepropEverySecond(void) {
-//   Tprop.current_time_secs++;    // increment time
-//   for (int i=0; i<TIMEPROP_NUM_OUTPUTS; i++) {
-//     int newState = Tprop.timeprops[i].tick(Tprop.current_time_secs);
-//     if (newState != bitRead(Tprop.current_relay_states, Tprop.relay_nos[i]-1)){
-//       // remove the third parameter below if using tasmota prior to v6.0.0
-//       ExecuteCommandPower(Tprop.relay_nos[i], newState,SRC_IGNORE);
-//     }
-//   }
-// }
-
-// // called by the system each time a relay state is changed
-// void TimepropXdrvPower(void) {
-//   // for a single relay the state is in the lsb of index, I have think that for
-//   // multiple outputs then succesive bits will hold the state but have not been
-//   // able to test that
-//   Tprop.current_relay_states = XdrvMailbox.index;
-// }
-
-// /* struct XDRVMAILBOX { */
-// /*   uint16_t      valid; */
-// /*   uint16_t      index; */
-// /*   uint16_t      data_len; */
-// /*   int16_t       payload; */
-// /*   char         *topic; */
-// /*   char         *data; */
-// /* } XdrvMailbox; */
-
-// // To get here post with topic cmnd/timeprop_setpower_n where n is index into timeprops 0:7
-// bool TimepropCommand()
-// {
-//   char command [CMDSZ];
-//   bool serviced = true;
-//   uint8_t ua_prefix_len = strlen(D_CMND_TIMEPROP); // to detect prefix of command
-//   /*
-//   AddLog(LOG_LEVEL_INFO, PSTR("Command called: "
-//     "index: %d data_len: %d payload: %d topic: %s data: %s"),
-//     XdrvMailbox.index,
-//     XdrvMailbox.data_len,
-//     XdrvMailbox.payload,
-//     (XdrvMailbox.payload >= 0 ? XdrvMailbox.topic : ""),
-//     (XdrvMailbox.data_len >= 0 ? XdrvMailbox.data : ""));
-//   */
-//   if (0 == strncasecmp_P(XdrvMailbox.topic, PSTR(D_CMND_TIMEPROP), ua_prefix_len)) {
-//     // command starts with timeprop_
-//     int command_code = GetCommandCode(command, sizeof(command), XdrvMailbox.topic + ua_prefix_len, kTimepropCommands);
-//     if (CMND_TIMEPROP_SETPOWER == command_code) {
-//       /*
-//       AddLog(LOG_LEVEL_INFO, PSTR("Timeprop command timeprop_setpower: "
-//         "index: %d data_len: %d payload: %d topic: %s data: %s"),
-// 	      XdrvMailbox.index,
-// 	      XdrvMailbox.data_len,
-// 	      XdrvMailbox.payload,
-// 	      (XdrvMailbox.payload >= 0 ? XdrvMailbox.topic : ""),
-// 	      (XdrvMailbox.data_len >= 0 ? XdrvMailbox.data : ""));
-//       */
-//       if (XdrvMailbox.index >=0 && XdrvMailbox.index < TIMEPROP_NUM_OUTPUTS) {
-//         timeprops[XdrvMailbox.index].setPower( atof(XdrvMailbox.data), Tprop.current_time_secs );
-//       }
-//       Response_P(PSTR("{\"" D_CMND_TIMEPROP D_CMND_TIMEPROP_SETPOWER "%d\":\"%s\"}"), XdrvMailbox.index, XdrvMailbox.data);
-//     }
-//     else {
-//       serviced = false;
-//     }
-//   } else {
-//     serviced = false;
-//   }
-//   return serviced;
-// }
-
-// /*********************************************************************************************\
-//  * Interface
-// \*********************************************************************************************/
-
-// #define XDRV_48       48
-
-// bool Xdrv48(byte function) {
-//   bool result = false;
-
-//   switch (function) {
-//     case FUNC_INIT:
-//       TimepropInit();
-//       break;
-//     case FUNC_EVERY_SECOND:
-//       TimepropEverySecond();
-//       break;
-//     case FUNC_COMMAND:
-//       result = TimepropCommand();
-//       break;
-//     case FUNC_SET_POWER:
-//       TimepropXdrvPower();
-//       break;
-//   }
-//   return result;
-// }
 
 #endif // FIRMWARE_MINIMAL
 #endif // USE_TIMEPROP
