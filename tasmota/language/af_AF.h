184ef5e015abb8eef769c3265ba03f5b53579c73
Frank Reimer <freimer@juniper.net>
2022-09-19 10:30:19 +0200

    Adding text to other languages

--- tasmota/language/af_AF.h
+++ tasmota/language/af_AF.h
@@ -1142,4 +1142,15 @@
 #define D_NEOPOOL_PH_LOW                  "te laag"
 #define D_NEOPOOL_PUMP_TIME_EXCEEDED      "pomptyd oorskry"
 
+#define D_CONFIGURE_TIMEPROP "Configure Timeprop"
+#define D_TIMEPROP_PARAMETERS "Timeprop parameters"
+#define D_TIMEPROP_FALLBACKAFTER "Fallback Hours"
+#define D_TIMEPROP_STARTUPFALLBACK "Start with Fallback"
+#define D_TIMEPROP_RELAY "Relay"
+#define D_TIMEPROP_TIMEBASE "Relay Circle Length"
+#define D_TIMEPROP_FALLBACK "Relay Fallback Value"
+#define D_TIMEPROP_MINUTES "Minutes"
+#define D_TIMEPROP_HOURS "Hours"
+#define D_TIMEPROP_PERCENT "0-100"
+
 #endif  // _LANGUAGE_AF_AF_H_
--- tasmota/language/el_GR.h
+++ tasmota/language/el_GR.h
@@ -1142,4 +1142,15 @@
 #define D_NEOPOOL_PH_LOW                  "too low"
 #define D_NEOPOOL_PUMP_TIME_EXCEEDED      "pump time exceeded"
 
+#define D_CONFIGURE_TIMEPROP "Configure Timeprop"
+#define D_TIMEPROP_PARAMETERS "Timeprop parameters"
+#define D_TIMEPROP_FALLBACKAFTER "Fallback Hours"
+#define D_TIMEPROP_STARTUPFALLBACK "Start with Fallback"
+#define D_TIMEPROP_RELAY "Relay"
+#define D_TIMEPROP_TIMEBASE "Relay Circle Length"
+#define D_TIMEPROP_FALLBACK "Relay Fallback Value"
+#define D_TIMEPROP_MINUTES "Minutes"
+#define D_TIMEPROP_HOURS "Hours"
+#define D_TIMEPROP_PERCENT "0-100"
+
 #endif  // _LANGUAGE_EL_GR_H_
